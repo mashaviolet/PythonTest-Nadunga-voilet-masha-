78e479db7d925ad238f3ed72d91f482ab2492842
Mashaviolet <violetmasha1819@gmail.com>
2024-10-24 11:21:59 +0300

    completed my test

---
+++ question3.py
@@ -0,0 +1,19 @@
+# Question 3(i)
+#  Write a Python program that prompts a user to enter numbers. The process will repeat until
+#  the user enters 0. Finally, the program prints sum of the numbers entered by the user.
+#SOLUTION
+
+number=int(input('Enter numbers :'))
+
+sum= 0
+sum=+number
+print(f"The sum of the numbers entered is: {sum}")
+
+while True:
+
+    number=int(input('Enter numbers :'))
+    if number == 0:
+     print('STOP')
+# Question 3(ii)
+# Write a Python program to print all the numbers from 1 to 100 that are not divisible by 2
+#solution
